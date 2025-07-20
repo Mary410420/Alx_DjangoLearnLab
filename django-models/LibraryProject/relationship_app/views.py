75ab4dfa429155eb259548eb15aa837d23083af0
Jembi Mary <Jembimary11@gmail.com>
2025-07-20 19:05:04 +0100

    Task 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,9 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
 
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render
+
 # Class-Based View: Display library detail
 class LibraryDetailView(DetailView):
     model = Library
@@ -14,3 +17,29 @@ class LibraryDetailView(DetailView):
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+@login_required
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+@login_required
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+@login_required
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
