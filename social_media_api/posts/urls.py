05f350ad2c952b96624ad990d71a6be4fb8d2e24
Jembi Mary <Jembimary11@gmail.com>
2025-08-31 20:13:02 +0100

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,4 +9,8 @@ router.register(r'comments', CommentViewSet, basename='comment')
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', feed, name='feed'),
+
+    # Explicit like/unlike URLs
+    path('posts/<int:pk>/like/', PostViewSet.as_view({'post': 'like'}), name='post-like'),
+    path('posts/<int:pk>/unlike/', PostViewSet.as_view({'post': 'unlike'}), name='post-unlike'),
 ]
