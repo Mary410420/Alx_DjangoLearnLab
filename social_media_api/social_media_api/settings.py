bad222275868e301201a29b69ede4ce806878ed1
Jembi Mary <Jembimary11@gmail.com>
2025-08-31 20:30:38 +0100

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,3 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = 'DENY'
 SECURE_SSL_REDIRECT = True  # Redirect all HTTP to HTTPS
+
+
+# Port for local or production deployment (Heroku provides PORT in env)
+PORT = int(os.environ.get('PORT', 8000))  # default to 8000 if not set
