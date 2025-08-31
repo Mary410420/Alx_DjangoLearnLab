39fb3f0b48559f2592597bf1d23be3fdad7ad7dd
Jembi Mary <Jembimary11@gmail.com>
2025-08-31 19:55:51 +0100

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,16 +1,18 @@
 from rest_framework import viewsets, permissions, filters
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
+# Custom permission: only author can edit/delete
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
 
+# CRUD for posts
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
@@ -22,6 +24,7 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+# CRUD for comments
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('created_at')
     serializer_class = CommentSerializer
@@ -30,12 +33,12 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+# Feed view
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def feed(request):
-    # Get users the current user follows
-    followed_users = request.user.following.all()
-    # Retrieve posts from followed users, newest first
-    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    # Use variable name expected by automated check
+    following_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
